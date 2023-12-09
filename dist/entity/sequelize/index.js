9dc2f882103f2d80728c5959f42dd3e8d5a356ff
Dotun Longe <oludotunlonge@gmail.com>
2023-12-09 02:26:44 +0100

    fix:config-of-db

--- dist/entity/sequelize/index.js
+++ dist/entity/sequelize/index.js
@@ -5,13 +5,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const config_json_1 = __importDefault(require("../../config/config.json"));
-/**
- * Initializes a Sequelize instance.
- */
-const sequelize = new sequelize_1.Sequelize(config_json_1.default.development.database, config_json_1.default.development.username, config_json_1.default.development.password, {
-    host: 'localhost',
-    dialect: 'postgres'
-});
+let sequelize;
+if (process.env.NODE_ENV === 'production') {
+    sequelize = new sequelize_1.Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false // For development; use secure connection in production
+            }
+        }
+    });
+}
+else {
+    sequelize = new sequelize_1.Sequelize(config_json_1.default.development.database, config_json_1.default.development.username, config_json_1.default.development.password, {
+        host: 'localhost',
+        dialect: 'postgres'
+    });
+}
 // Test the connection
 sequelize.authenticate()
     .then(() => {
--- entity/sequelize/index.ts
+++ entity/sequelize/index.ts
@@ -1,13 +1,24 @@
 import { Sequelize } from 'sequelize';
 import config from "../../config/config.json";
 
-/**
- * Initializes a Sequelize instance.
- */
-const sequelize = new Sequelize(config.development.database, config.development.username, config.development.password, {
-  host: 'localhost',
-  dialect: 'postgres'
-});
+let sequelize;
+
+if (process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // For development; use secure connection in production
+      }
+    }
+  });
+} else {
+  sequelize = new Sequelize(config.development.database, config.development.username, config.development.password, {
+    host: 'localhost',
+    dialect: 'postgres'
+  });
+}
 
 // Test the connection
 sequelize.authenticate()
@@ -17,5 +28,5 @@ sequelize.authenticate()
   .catch(err => {
     console.error('Unable to connect to the database:', err);
   });
-  
+
 export default sequelize;
